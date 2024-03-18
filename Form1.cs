332c373ffa1595405e76dec7882fae58efc10390
Davi-A-Gonca <127240455+Davi-A-Gonca@users.noreply.github.com>
2024-03-18 15:20:48 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,320 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Banco_De_Dados_1
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection Conexao;
+        private string database_source = "datasource=localhost;username=root;password=16112809;database=botecoDoSeuZe";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            AtualizarPedidos();
+            AtualizarVendiveis();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void AtualizarPedidos()
+        {
+            lstVw1.Items.Clear();
+            try
+            {
+                Conexao = new MySqlConnection(database_source);
+
+                string Mostrar = "SELECT * FROM pedido";
+
+                Conexao.Open();
+
+                MySqlCommand command = new MySqlCommand(Mostrar, Conexao);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string[] pedido = reader.GetString(1).Split("@");
+                    string[] row = {
+                        reader.GetValue(0).ToString(),
+                        pedido[0],
+                        pedido[1],
+                        reader.GetValue(2).ToString(),
+                    };
+                    var linha_lstVw1 = new ListViewItem(row);
+                    lstVw1.Items.Add(linha_lstVw1);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+
+        public void AtualizarVendiveis()
+        {
+            try
+            {
+                lstVw2.Items.Clear();
+                Conexao = new MySqlConnection(database_source);
+
+                Conexao.Open();
+
+                string MostrarConsumiveis = "SELECT * FROM consumiveis";
+
+                MySqlCommand commandConsumiveis = new MySqlCommand(MostrarConsumiveis, Conexao);
+                MySqlDataReader readerConsumiveis = commandConsumiveis.ExecuteReader();
+
+                while (readerConsumiveis.Read())
+                {
+                    string[] row = {
+                        readerConsumiveis.GetValue(0).ToString(),
+                        readerConsumiveis.GetString(1),
+                        readerConsumiveis.GetValue(2).ToString(),
+                        readerConsumiveis.GetValue(3).ToString(),
+                    };
+                    var linha_lstVw2 = new ListViewItem(row);
+                    lstVw2.Items.Add(linha_lstVw2);
+                    if (Int16.Parse(readerConsumiveis.GetValue(3).ToString()) > 0)
+                    {
+                        lstBxVendiveis.Items.Add(readerConsumiveis.GetString(1));
+                    }
+                }
+
+                Conexao.Close();
+                Conexao.Open();
+
+                string MostrarReceitas = "SELECT * FROM receitas";
+
+                MySqlCommand commandReceitas = new MySqlCommand(MostrarReceitas, Conexao);
+                MySqlDataReader readerReceitas = commandReceitas.ExecuteReader();
+
+                while (readerReceitas.Read())
+                {
+                    string[] row = {
+                        readerReceitas.GetValue(0).ToString(),
+                        readerReceitas.GetString(1),
+                        "N/A",
+                        readerReceitas.GetValue(3).ToString(),
+                    };
+                    var linha_lstVw2 = new ListViewItem(row);
+                    lstVw2.Items.Add(linha_lstVw2);
+                    lstBxVendiveis.Items.Add(readerReceitas.GetString(1));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+
+        private void btnAdicionarPedido_Click(object sender, EventArgs e)
+        {
+            if (txtBxIdentificadorCliente.Text == "")
+            {
+                MessageBox.Show("Preencha todos os campos necessarios\n(\"Cliente\")", "Erro de preenchimento", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if (txtBxPedido.Text == "")
+            {
+                MessageBox.Show("Preencha todos os campos necessarios\n(\"Pedido\")", "Erro de preenchimento", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if(txtBxQuantidadeDePedido.Text == "")
+            {
+                MessageBox.Show("Preencha todos os campos necessarios\n(\"Quantidade\")", "Erro de preenchimento", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string pedido = txtBxPedido.Text + "@" + txtBxQuantidadeDePedido.Text + "@";
+            int ID_cliente = CodigoCliente(txtBxIdentificadorCliente.Text);
+            try
+            {
+                Conexao = new MySqlConnection(database_source);
+
+                Conexao.Open();
+
+                if (ID_cliente < 0)
+                {
+                    MySqlCommand commandCliente = new MySqlCommand();
+                    commandCliente.Connection = Conexao;
+                    commandCliente.CommandText = "INSERT INTO cliente (Identificador) VALUES (@identificador);";
+                    commandCliente.Parameters.Clear();
+                    commandCliente.Parameters.AddWithValue("@identificador", txtBxIdentificadorCliente.Text);
+                    commandCliente.ExecuteNonQuery();
+                    ID_cliente = CodigoCliente(txtBxIdentificadorCliente.Text);
+                }
+
+                MySqlCommand command = new MySqlCommand();
+                command.Connection = Conexao;
+                command.CommandText = "INSERT INTO pedido (Pedido, Cliente) VALUES (@pedido, @cliente);";
+                command.Parameters.Clear();
+                command.Parameters.AddWithValue("@pedido", pedido);
+                command.Parameters.AddWithValue("@cliente", ID_cliente);
+
+                command.ExecuteNonQuery();
+                AtualizarPedidos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+                txtBxQuantidadeDePedido.Text = "";
+                txtBxPedido.Text = "";
+                txtBxIdentificadorCliente.Text = "";
+            }
+        }
+
+        private void btnAdicionarVendivel_Click(object sender, EventArgs e)
+        {
+            Form2 newform = new Form2();
+            newform.Show();
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            AtualizarVendiveis();
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            int idRemover = Int32.Parse(lstVw1.SelectedItems[0].Text);
+            try
+            {
+                Conexao = new MySqlConnection(database_source);
+
+                Conexao.Open();
+                MySqlCommand command = new MySqlCommand();
+                command.Connection = Conexao;
+                command.CommandText = "DELETE FROM pedido WHERE ID_Pedido=@id;";
+                command.Parameters.Clear();
+                command.Parameters.AddWithValue("@id", idRemover);
+                command.ExecuteNonQuery();
+                AtualizarPedidos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+
+        }
+
+        private void btnBuscarCliente_Click(object sender, EventArgs e)
+        {
+            lstVw1.Items.Clear();
+            if (txtBxIdentificadorCliente.Text == "")
+            {
+                MessageBox.Show("Preencha todos os campos necessarios\n(\"Cliente\")", "Erro de preenchimento", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                int idCliente = CodigoCliente(txtBxIdentificadorCliente.Text);
+                Conexao = new MySqlConnection(database_source);
+                Conexao.Open();
+
+                MySqlCommand command = new MySqlCommand();
+                command.Connection = Conexao;
+                command.CommandText = "SELECT * FROM pedido WHERE Cliente=@idCliente;";
+                command.Parameters.Clear();
+                command.Parameters.AddWithValue("@idCliente", idCliente);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string[] pedido = reader.GetString(1).Split("@");
+                    string[] row = {
+                        reader.GetValue(0).ToString(),
+                        pedido[0],
+                        pedido[1],
+                        reader.GetValue(2).ToString(),
+                    };
+                    var linha_lstVw1 = new ListViewItem(row);
+                    lstVw1.Items.Add(linha_lstVw1);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+
+        private int CodigoCliente(string identificadorCliente)
+        {
+            try
+            {
+                int quantosTem = 0;
+                int codigoCliente = 0;
+
+                Conexao = new MySqlConnection(database_source);
+
+                Conexao.Open();
+
+                MySqlCommand commandChecar = new MySqlCommand();
+                commandChecar.Connection = Conexao;
+                commandChecar.CommandText = "SELECT COUNT(Identificador) FROM cliente WHERE Identificador=@identificador;";
+                commandChecar.Parameters.Clear();
+                commandChecar.Parameters.AddWithValue("@identificador", identificadorCliente);
+                MySqlDataReader readerChecar = commandChecar.ExecuteReader();
+
+                while (readerChecar.Read())
+                {
+                    quantosTem = Int32.Parse(readerChecar.GetValue(0).ToString());
+                }
+
+                if( quantosTem > 0 )
+                {
+
+                    Conexao = new MySqlConnection(database_source);
+
+                    Conexao.Open();
+
+                    MySqlCommand commandCodigo = new MySqlCommand();
+                    commandCodigo.Connection = Conexao;
+                    commandCodigo.CommandText = "SELECT * FROM cliente WHERE Identificador=@identificador;";
+                    commandCodigo.Parameters.Clear();
+                    commandCodigo.Parameters.AddWithValue("@identificador", identificadorCliente);
+                    MySqlDataReader readerCodigo = commandCodigo.ExecuteReader();
+
+                    while (readerCodigo.Read())
+                    {
+                        codigoCliente = Int32.Parse(readerCodigo.GetValue(0).ToString());
+                    }
+                    return codigoCliente;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+            return -1;
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,122 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banco_De_Dados_1
+{
+    public partial class Form2 : Form
+    {
+        MySqlConnection Conexao2;
+        private string database_source = "datasource=localhost;username=root;password=16112809;database=botecoDoSeuZe";
+        private Form1 form1 = new Form1();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdicionarConsumivel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string descricao = txtBxDescricaoConsumivel.Text;
+                int quantidade = Int32.Parse(txtBxQuantidadeConsumivel.Text);
+                double valor = Double.Parse(txtBxValorConsumivel.Text);
+                Conexao2 = new MySqlConnection(database_source);
+
+                Conexao2.Open();
+
+                MySqlCommand commandVendivel = new MySqlCommand();
+                commandVendivel.Connection = Conexao2;
+                commandVendivel.CommandText = "INSERT INTO consumiveis (ID_Consumivel, Descricao, Quantidade, Valor) VALUES (@id, @descricao, @quantidade, @valor);";
+                commandVendivel.Parameters.Clear();
+                commandVendivel.Parameters.AddWithValue("@id", 1);
+                commandVendivel.Parameters.AddWithValue("@descricao", descricao);
+                commandVendivel.Parameters.AddWithValue("@quantidade", quantidade);
+                commandVendivel.Parameters.AddWithValue("@valor", valor);
+                commandVendivel.ExecuteNonQuery();
+                form1.AtualizarVendiveis();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao2.Close();
+            }
+
+        }
+
+        private void btnAdicionarIngrediente_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string descricao = txtBxNomeIngrediente.Text;
+                int quantidade = Int32.Parse(txtBxQuantidadeIngrediente.Text);
+                Conexao2 = new MySqlConnection(database_source);
+
+                Conexao2.Open();
+
+                MySqlCommand commandIngredientes = new MySqlCommand();
+                commandIngredientes.Connection = Conexao2;
+                commandIngredientes.CommandText = "INSERT INTO consumiveis (ID_Consumivel, Descricao, Quantidade, Valor) VALUES (@id, @descricao, @quantidade, @valor);";
+                commandIngredientes.Parameters.Clear();
+                commandIngredientes.Parameters.AddWithValue("@id", 2);
+                commandIngredientes.Parameters.AddWithValue("@descricao", descricao);
+                commandIngredientes.Parameters.AddWithValue("@quantidade", quantidade);
+                commandIngredientes.Parameters.AddWithValue("@valor", 0);
+                commandIngredientes.ExecuteNonQuery();
+                form1.AtualizarVendiveis();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao2.Close();
+            }
+
+        }
+
+        private void btnAdicionarReceita_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nome = txtBxNomeReceita.Text;
+                string ingredientes = txtBxIngredientesReceita.Text;
+                double valor = Double.Parse(txtBxValorReceita.Text);
+                Conexao2 = new MySqlConnection(database_source);
+
+                Conexao2.Open();
+
+                MySqlCommand commandReceita = new MySqlCommand();
+                commandReceita.Connection = Conexao2;
+                commandReceita.CommandText = "INSERT INTO receitas (ID_Receitas, Nome, Ingredientes, Valor) VALUES (@id, @nome, @ingredientes, @valor);";
+                commandReceita.Parameters.Clear();
+                commandReceita.Parameters.AddWithValue("@id", 3);
+                commandReceita.Parameters.AddWithValue("@nome", nome);
+                commandReceita.Parameters.AddWithValue("@ingredientes", ingredientes);
+                commandReceita.Parameters.AddWithValue("@valor", valor);
+                commandReceita.ExecuteNonQuery();
+                form1.AtualizarVendiveis();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao2.Close();
+            }
+
+        }
+    }
+}
