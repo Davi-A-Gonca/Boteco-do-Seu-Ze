0bd2c093de754bc8207c938c35b28c9f2b6d3d9c
Davi-A-Gonca <127240455+Davi-A-Gonca@users.noreply.github.com>
2024-03-23 16:34:41 -0300

    Update Form2.cs

--- Form2.cs
+++ Form2.cs
@@ -1,4 +1,4 @@
-﻿using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -23,6 +23,24 @@ namespace Banco_De_Dados_1
 
         private void btnAdicionarConsumivel_Click(object sender, EventArgs e)
         {
+            if (txtBxDescricaoConsumivel.Text == "")
+            {
+                MessageBox.Show("Preencha todos os campos necessários (\"Descrição\")", "Campos obrigatórios não preenchidos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtBxDescricaoConsumivel.Focus();
+                return;
+            }
+            else if (txtBxQuantidadeConsumivel.Text == "")
+            {
+                MessageBox.Show("Preencha todos os campos necessários (\"Quantidade\")", "Campos obrigatórios não preenchidos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtBxQuantidadeConsumivel.Focus();
+                return;
+            }
+            else if (txtBxValorConsumivel.Text == "")
+            {
+                MessageBox.Show("Preencha todos os campos necessários (\"Valor\")", "Campos obrigatórios não preenchidos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtBxValorConsumivel.Focus();
+                return;
+            }
             try
             {
                 string descricao = txtBxDescricaoConsumivel.Text;
@@ -34,9 +52,8 @@ namespace Banco_De_Dados_1
 
                 MySqlCommand commandVendivel = new MySqlCommand();
                 commandVendivel.Connection = Conexao2;
-                commandVendivel.CommandText = "INSERT INTO consumiveis (ID_Consumivel, Descricao, Quantidade, Valor) VALUES (@id, @descricao, @quantidade, @valor);";
+                commandVendivel.CommandText = "INSERT INTO consumiveis (Descricao, Quantidade, Valor) VALUES (@descricao, @quantidade, @valor);";
                 commandVendivel.Parameters.Clear();
-                commandVendivel.Parameters.AddWithValue("@id", 1);
                 commandVendivel.Parameters.AddWithValue("@descricao", descricao);
                 commandVendivel.Parameters.AddWithValue("@quantidade", quantidade);
                 commandVendivel.Parameters.AddWithValue("@valor", valor);
@@ -56,6 +73,18 @@ namespace Banco_De_Dados_1
 
         private void btnAdicionarIngrediente_Click(object sender, EventArgs e)
         {
+            if (txtBxNomeIngrediente.Text == "")
+            {
+                MessageBox.Show("Preencha todos os campos necessários (\"Nome\")", "Campos obrigatórios não preenchidos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtBxNomeIngrediente.Focus();
+                return;
+            }
+            else if (txtBxQuantidadeIngrediente.Text == "")
+            {
+                MessageBox.Show("Preencha todos os campos necessários (\"Quantidade\")", "Campos obrigatórios não preenchidos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtBxQuantidadeIngrediente.Focus();
+                return;
+            }
             try
             {
                 string descricao = txtBxNomeIngrediente.Text;
@@ -66,9 +95,8 @@ namespace Banco_De_Dados_1
 
                 MySqlCommand commandIngredientes = new MySqlCommand();
                 commandIngredientes.Connection = Conexao2;
-                commandIngredientes.CommandText = "INSERT INTO consumiveis (ID_Consumivel, Descricao, Quantidade, Valor) VALUES (@id, @descricao, @quantidade, @valor);";
+                commandIngredientes.CommandText = "INSERT INTO consumiveis (Descricao, Quantidade, Valor) VALUES (@descricao, @quantidade, @valor);";
                 commandIngredientes.Parameters.Clear();
-                commandIngredientes.Parameters.AddWithValue("@id", 2);
                 commandIngredientes.Parameters.AddWithValue("@descricao", descricao);
                 commandIngredientes.Parameters.AddWithValue("@quantidade", quantidade);
                 commandIngredientes.Parameters.AddWithValue("@valor", 0);
@@ -88,10 +116,34 @@ namespace Banco_De_Dados_1
 
         private void btnAdicionarReceita_Click(object sender, EventArgs e)
         {
+            if (txtBxNomeReceita.Text == "")
+            {
+                MessageBox.Show("Preencha todos os campos necessários (\"Nome\")", "Campos obrigatórios não preenchidos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtBxNomeReceita.Focus();
+                return;
+            }
+            else if (txtBxValorReceita.Text == "")
+            {
+                MessageBox.Show("Preencha todos os campos necessários (\"Valor\")", "Campos obrigatórios não preenchidos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtBxValorReceita.Focus();
+                return;
+            }
+            else if (lstVwIngredientes.Items.Count == 0)
+            {
+                MessageBox.Show("Ingredientes para a receita não encontrados", "Campos obrigatórios não preenchidos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
             try
             {
                 string nome = txtBxNomeReceita.Text;
-                string ingredientes = txtBxIngredientesReceita.Text;
+                string ingredientes = "";
+                for (int i = 0; i < lstVwIngredientes.Items.Count; i++)
+                {
+                    ingredientes += lstVwIngredientes.Items[i].SubItems[0].Text;
+                    ingredientes += "§";
+                    ingredientes += lstVwIngredientes.Items[i].SubItems[1].Text;
+                    ingredientes += "§";
+                }
                 double valor = Double.Parse(txtBxValorReceita.Text);
                 Conexao2 = new MySqlConnection(database_source);
 
@@ -99,9 +151,8 @@ namespace Banco_De_Dados_1
 
                 MySqlCommand commandReceita = new MySqlCommand();
                 commandReceita.Connection = Conexao2;
-                commandReceita.CommandText = "INSERT INTO receitas (ID_Receitas, Nome, Ingredientes, Valor) VALUES (@id, @nome, @ingredientes, @valor);";
+                commandReceita.CommandText = "INSERT INTO receitas (Nome, Ingredientes, Valor) VALUES (@nome, @ingredientes, @valor);";
                 commandReceita.Parameters.Clear();
-                commandReceita.Parameters.AddWithValue("@id", 3);
                 commandReceita.Parameters.AddWithValue("@nome", nome);
                 commandReceita.Parameters.AddWithValue("@ingredientes", ingredientes);
                 commandReceita.Parameters.AddWithValue("@valor", valor);
@@ -117,6 +168,28 @@ namespace Banco_De_Dados_1
                 Conexao2.Close();
             }
 
+        }
+
+        private void btnAdicionarIngredienteALista_Click(object sender, EventArgs e)
+        {
+            if (txtBxIngredientesReceita.Text == "")
+            {
+                MessageBox.Show("Preencha todos os campos necessários (\"Ingrediente\")", "Campos obrigatórios não preenchidos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtBxIngredientesReceita.Focus();
+                return;
+            }else if(txtBxQuantidade.Text == "")
+            {
+                MessageBox.Show("Preencha todos os campos necessários (\"Quantidade\")", "Campos obrigatórios não preenchidos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtBxQuantidade.Focus();
+                return;
+            }
+            string[] row =
+            {
+                txtBxIngredientesReceita.Text,
+                txtBxQuantidade.Text,
+            };
+            var linha_lstVw = new ListViewItem(row);
+            lstVwIngredientes.Items.Add(linha_lstVw);
         }
     }
 }
